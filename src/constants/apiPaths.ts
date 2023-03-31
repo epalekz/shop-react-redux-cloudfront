991386b8772ba12001bd3df5b622291bebf47cf1
epalekz <alejandro_aragon@epam.com>
2023-03-30 23:35:21 -0600

    updating url and links to lambda functions

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,12 +1,14 @@
 let apiGateway = import.meta.env.VITE_API_GATEWAY_ID;
+let apiGateway2 = import.meta.env.VITE_API_GATEWAY_ID2;
 apiGateway = "6kjrpwj3v8";
+apiGateway2 = "hfs91lpfsc";
 
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://.execute-api.us-east-1.amazonaws.com/dev",
+  order: "https://.execute-api.us-east-1.amazonaws.com/dev",
+  import: `https://${apiGateway2}.execute-api.us-east-1.amazonaws.com/dev`,
   bff: `https://${apiGateway}.execute-api.us-east-1.amazonaws.com`,
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://.execute-api.us-east-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
